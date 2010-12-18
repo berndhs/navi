e0237e855e79df49a15dd122a3a13e9850522fbe
Bernd Stramm <bernd.stramm@gmail.com>
2010-12-17 20:36:47 -0500

    initial

---
+++ src/navi.cpp
@@ -0,0 +1,221 @@
+#include "navi.h"
+
+/****************************************************************
+ * This file is distributed under the following license:
+ *
+ * Copyright (C) 2010, Bernd Stramm
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Boston, MA  02110-1301, USA.
+ ****************************************************************/
+
+#include <QApplication>
+#include "deliberate.h"
+#include "version.h"
+#include "helpview.h"
+#include <QSize>
+#include <QDebug>
+#include <QMessageBox>
+#include <QTimer>
+#include <QCursor>
+#include <QNetworkRequest>
+
+using namespace deliberate;
+
+namespace navi
+{
+
+Navi::Navi (QWidget *parent)
+  :QMainWindow (parent),
+   initDone (false),
+   app (0),
+   configEdit (this),
+   helpView (0),
+   runAgain (false),
+   network (this)
+{
+  mainUi.setupUi (this);
+  mainUi.actionRestart->setEnabled (false);
+  helpView = new HelpView (this);
+  Connect ();
+}
+
+void
+Navi::Init (QApplication &ap)
+{
+  app = &ap;
+  connect (app, SIGNAL (lastWindowClosed()), this, SLOT (Exiting()));
+  Settings().sync();
+  initDone = true;
+}
+
+bool
+Navi::Again ()
+{
+  bool again = runAgain;
+  runAgain = false;
+  return again;
+}
+
+bool
+Navi::Run ()
+{
+  runAgain = false;
+  if (!initDone) {
+    Quit ();
+    return false;
+  }
+  qDebug () << " Start Navi";
+  QSize defaultSize = size();
+  QSize newsize = Settings().value ("sizes/main", defaultSize).toSize();
+  resize (newsize);
+  Settings().setValue ("sizes/main",newsize);
+  show ();
+  return true;
+}
+
+void
+Navi::Connect ()
+{
+  connect (mainUi.actionQuit, SIGNAL (triggered()), 
+           this, SLOT (Quit()));
+  connect (mainUi.actionSettings, SIGNAL (triggered()),
+           this, SLOT (EditSettings()));
+  connect (mainUi.actionAbout, SIGNAL (triggered()),
+           this, SLOT (About ()));
+  connect (mainUi.actionLicense, SIGNAL (triggered()),
+           this, SLOT (License ()));
+  connect (mainUi.actionRestart, SIGNAL (triggered()),
+           this, SLOT (Restart ()));
+  connect (mainUi.readButton, SIGNAL (clicked()),
+           this, SLOT (ReadButton ()));
+
+  connect (&network, SIGNAL (finished (QNetworkReply*)),
+           this, SLOT (HandleReply (QNetworkReply*)));
+}
+
+void
+Navi::Restart ()
+{
+  qDebug () << " restart called ";
+  runAgain = true;
+  Quit ();
+}
+
+
+void
+Navi::Quit ()
+{
+  CloseCleanup ();
+  if (app) {
+    app->quit();
+  }
+}
+
+void
+Navi::closeEvent (QCloseEvent *event)
+{
+  Q_UNUSED (event)
+  CloseCleanup ();
+}
+
+void
+Navi::CloseCleanup ()
+{
+  QSize currentSize = size();
+  Settings().setValue ("sizes/main",currentSize);
+  Settings().sync();
+}
+
+void
+Navi::EditSettings ()
+{
+  configEdit.Exec ();
+  SetSettings ();
+}
+
+void
+Navi::SetSettings ()
+{
+  Settings().sync ();
+}
+
+void
+Navi::About ()
+{
+  QString version (deliberate::ProgramVersion::Version());
+  QStringList messages;
+  messages.append (version);
+  messages.append (configMessages);
+
+  QMessageBox  box;
+  box.setText (version);
+  box.setDetailedText (messages.join ("\n"));
+  QTimer::singleShot (30000, &box, SLOT (accept()));
+  box.exec ();
+}
+
+void
+Navi::Exiting ()
+{
+  QSize currentSize = size();
+  Settings().setValue ("sizes/main",currentSize);
+  Settings().sync();
+}
+
+void
+Navi::License ()
+{
+  if (helpView) {
+    helpView->Show ("qrc:/help/LICENSE.txt");
+  }
+}
+
+void
+Navi::ReadButton ()
+{
+  double lat = mainUi.latEdit->text().toDouble ();
+  double lon = mainUi.lonEdit->text().toDouble ();
+  QUrl url;
+  url.setScheme ("http");
+  url.setHost ("xapi.openstreetmap.org");
+  url.setPath ("api/0.6/map");
+  double partMinute = 1.0/240.0;
+  double left = lon - partMinute;
+  double right = lon + partMinute;
+  double top = lat + partMinute;
+  double bot = lat - partMinute;
+  url.addQueryItem (QString ("bbox"),QString ("%1,%2,%3,%4")
+                                      .arg (left).arg (bot)
+                                      .arg (right).arg (top));
+  
+  QNetworkRequest  req (url);
+  reply = network.get (req);
+  mainUi.logDisplay->append (QString ("requested: %1")
+                               .arg (req.url().toString()));
+}
+
+void
+Navi::HandleReply (QNetworkReply * reply)
+{
+  QByteArray  data = reply->readAll ();
+  mainUi.logDisplay->append ("Reply to ");
+  mainUi.logDisplay->append (reply->url().toString());
+  mainUi.logDisplay->append (QString (data));
+}
+
+
+} // namespace
+
---
+++ src/navi.h
@@ -0,0 +1,91 @@
+#ifndef NAVI_H
+#define NAVI_H
+
+/****************************************************************
+ * This file is distributed under the following license:
+ *
+ * Copyright (C) 2010, Bernd Stramm
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Boston, MA  02110-1301, USA.
+ ****************************************************************/
+#include <QMainWindow>
+#include "ui_navi.h"
+#include "config-edit.h"
+#include "helpview.h"
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+class QApplication;
+
+using namespace deliberate;
+
+namespace navi 
+{
+
+class Navi : public QMainWindow
+{
+Q_OBJECT
+
+public:
+
+  Navi (QWidget *parent=0);
+
+  void  Init (QApplication &ap);
+  bool  Run ();
+  bool  Again ();
+
+  void  AddConfigMessages (const QStringList & cm) 
+           { configMessages.append (cm); }
+
+  void closeEvent ( QCloseEvent *event);
+
+private slots:
+
+  void Quit ();
+  void Restart ();
+  void EditSettings ();
+  void SetSettings ();
+  void About ();
+  void License ();
+  void Exiting ();
+
+  void ReadButton ();
+  void HandleReply (QNetworkReply *reply);
+
+private:
+
+  void Connect ();
+  void CloseCleanup ();
+
+  bool             initDone;
+  QApplication    *app;
+  Ui_NaviMain    mainUi;
+ 
+  ConfigEdit       configEdit;
+  QStringList      configMessages;
+
+  deliberate::HelpView        *helpView;
+  bool             runAgain;
+
+  QNetworkAccessManager   network;
+  QNetworkReply          *reply;
+
+};
+
+} // namespace
+
+#endif
